fcdcbdf08ca0bcc770e183533f7617e55db0f125
ManPanchani <manpanchani0511@gmail.com>
2023-01-31 15:13:24 +0530

    BMI Calculator

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:animated_bmi_app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    ),
+  );
+}
